556e98257189bbc3b9becc7d42aae01854b211b2
Sai Chandra Mouli Tadiparthi <cb.en.u4cse17160@cb.students.amrita.edu>
2020-04-13 22:29:23 +0530

    daily update of med resources at Mon Apr 13 22:29:23 IST 2020

---
+++ runner.sh
@@ -0,0 +1,13 @@
+echo "starting job@",$(date)
+token="f5230155b2e7da3fe89e63ff26a072254d1db923"
+
+git pull https://github.com/VICS-CORE/covid-api.git && cd covid-api
+
+node csv_to_json.js
+
+git add .
+git commit -m "daily update of med resources at $(date)"
+
+git push remote origin
+
+echo "Done pushing -- please check"
