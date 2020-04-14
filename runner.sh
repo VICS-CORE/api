d7728e6cba7b51f064976ce15ab24f45c6e4f9c5
VICS-ORG <epuser@ibrmsvr2>
2020-04-15 01:18:04 +0530

    daily update of med resources at Wed Apr 15 01:18:04 IST 2020

--- runner.sh
+++ runner.sh
@@ -5,7 +5,7 @@ token=""
 
 cd covid-api && git pull git@github.com:VICS-CORE/covid-api.git
 
-nodejs csv_to_json.js
+/home/epuser/node-v12.16.2/bin/node csv_to_json.js
 
 git add .
 git commit -m "daily update of med resources at $(date)"
