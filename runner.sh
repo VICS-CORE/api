b7b8ff7a8b7b4559b46ce0579244d08385cbd0c5
Mayank Bhagya <mayankbhagya@gmail.com>
2020-07-07 00:15:31 +0530

    Update predictions.json as well

--- runner.sh
+++ runner.sh
@@ -2,11 +2,11 @@
 echo "Generating statewise predictions", $(date)
 
 pushd covid-net
-python -m scripts.gen_statewise_predictions 0001 latest-e1740.pt -d 200 -vpf ../covid-api/vp/1.1740.json
+python -m scripts.gen_statewise_predictions 0001 latest-e1740.pt -d 200 -vpf ../covid-api/vp/1.1740.json -trf ../covid-api/predictions.json
 popd
 
 pushd covid-api
-git add vp
+git add vp predictions.json
 git commit -m "Daily update to 1.1740"
 git push origin master
 popd
