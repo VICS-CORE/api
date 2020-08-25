284899f2a52ca8ddf0748c144d21dc4d0d5e2430
jupyter1 <indiacovidseva@gmail.com>
2020-08-25 17:35:01 +0000

    Update model being used to 23.1155

--- runner.sh
+++ runner.sh
@@ -2,12 +2,12 @@
 echo "Generating statewise predictions", $(date)
 
 pushd covid-net
-/opt/conda/bin/python -m scripts.gen_statewise_predictions 0001 latest-e1740.pt -d 200 -vpf ../covid-api/vp/1.1740.json -trf ../covid-api/predictions.json
+/opt/conda/bin/python -m scripts.gen_statewise_predictions 0023 latest-e1155.pt -d 200 -vpf ../covid-api/vp/23.1155.json -trf ../covid-api/predictions.json
 popd
 
 pushd covid-api
 git add vp predictions.json
-git commit -m "Daily update to 1.1740"
+git commit -m "Daily update to 23.1155"
 git push origin master
 popd
 
