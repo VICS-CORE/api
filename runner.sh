21320720baf2d6ee40f3b4256f071b92beb1aa66
jupyter1 <indiacovidseva@gmail.com>
2020-09-02 16:00:41 +0000

    Updated model to 1.1740

--- runner.sh
+++ runner.sh
@@ -2,7 +2,7 @@
 echo "Generating statewise predictions", $(date)
 
 pushd covid-net
-/opt/conda/bin/python -m scripts.gen_statewise_predictions 0023 latest-e1155.pt -d 200 -vpf ../covid-api/vp/23.1155.json -trf ../covid-api/predictions.json
+/opt/conda/bin/python -m scripts.gen_statewise_predictions 0001 latest-e1740.pt -d 200 -vpf ../covid-api/vp/1.1740.json -trf ../covid-api/predictions.json
 popd
 
 pushd covid-api
